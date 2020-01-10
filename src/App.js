b9bf2e84a165428e56922ad7ddffc68c1ccf2dd4
SimJunSik <wnstlr24@naver.com>
2020-01-10 16:55:21 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styled, { css, ThemeProvider } from 'styled-components';
+import Button from './components/Button';
+import Dialog from './components/Dialog';
+
+const Circle = styled.div`
+  width: 5rem;
+  height: 5rem;
+  background: ${props => props.color};
+  border-radius: 50%;
+  ${props => props.huge &&
+    // 그냥 template literal을 사용하면 props를 또 받아오지 못함
+    // 그래서 { css } 를 사용
+    css`
+    width: 10rem;
+    height: 10rem;
+  `}
+`;
+
+const AppBlock = styled.div`
+  width: 512px;
+  margin: 0 auto;
+  margin-top: 4rem;
+  border: 1px solid black;
+  padding: 1rem;
+`;
+
+const ButtonGroup = styled.div`
+  & + & {
+    margin-top: 1rem;
+  }
+`;
+
+const palette = {
+  blue: '#228be6',
+  gray: '#496057',
+  pink: '#f06595'
+}
 
 function App() {
+  const [dialog, setDialog] = useState(false);
+  const onClick = () => {
+    setDialog(true);
+  }
+  const onConfirm = () => {
+    console.log("확인");
+    setDialog(false);
+  }
+  const onCancel = () => {
+    console.log("취소");
+    setDialog(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ThemeProvider theme={{ palette }}>
+      <AppBlock>
+        <ButtonGroup>
+          <Button size="large" color="pink">BUTTON</Button>
+          <Button>BUTTON</Button>
+          <Button size="small" color="gray">BUTTON</Button>
+        </ButtonGroup>
+        <ButtonGroup>
+          <Button size="large" color="pink" outline>BUTTON</Button>
+          <Button outline>BUTTON</Button>
+          <Button size="small" color="gray" outline>BUTTON</Button>
+        </ButtonGroup>
+        <ButtonGroup>
+          <Button size="large" color="pink" fullWidth>BUTTON</Button>
+          <Button size="large" fullWidth>BUTTON</Button>
+          <Button size="large" color="gray" fullWidth>BUTTON</Button>
+        </ButtonGroup>
+        <Button color="pink" size="large" onClick={onClick}>삭제</Button>
+        <Dialog
+          title="정말로 삭제하시겠습니까?"
+          confirmText="삭제"
+          cancelText="취소"
+          visible={dialog}
+          onConfirm={onConfirm}
+          onCancel={onCancel}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          데이터를 정말로 삭제하시겠습니까?
+        </Dialog>
+      </AppBlock>
+    </ThemeProvider>
   );
 }
 
